9f447ffb6a399bc7c479633788e90a8727c19cbc
milo <milolx@gmail.com>
2014-09-01 09:10:37 +0800

    merging, compile done

--- test.c
+++ test.c
@@ -1,6 +1,7 @@
-<<<<<<< HEAD
+#include <stdio.h>
+#include <pthread.h>
+#include "compact.h"
 #include "split.h"
-#include "rand.h"
 #include "rand.h"
 
 /*
@@ -13,179 +14,62 @@ void split_init();
 void split_cleanup();
 */
 
-#define TOT_SIZE	(50*1000*1000)
-#define GEN_ERR		1
-//#undef GEN_ERR
-//#define VERBOSE	1
-
-#ifdef EVALUATION
-extern unsigned long long e_valid;
-extern unsigned long long e_tot;
-#endif
-
-int main()
-{
-	u8 s[LINK_DATA_MAX];
-	u8 r[sizeof s];
-	u8 low[LINK_MTU];
-	int len;
-	int ret;
-	int n;
-	int fail = 0;
-	bool again = true;
-	unsigned long long tot = 0;
-
-	split_init();
-
-	while (tot < TOT_SIZE) {
-		// generate a random length of a packet[0..LINK_DATA_MAX]
-		n = random_range(LINK_DATA_MAX);
-		if (!n)
-			continue;
-		if (tot + n > TOT_SIZE)
-			n = TOT_SIZE - tot;
-		// generate the pkt of length n
-		random_bytes(s, n);
-#ifdef VERBOSE
-		printf("-------------\n");
-
-		printf("\nsend buf\n");
-		//hex_dump(stdout, s, n, 0, true);
-#endif
-		n = upper_send(2, 1, s, n);
-		tot += n;
-		printf("tot=%llu\n", tot);
-		// the return value 'n' is the actural submitted length
-#ifdef VERBOSE
-		printf("%d bytes accepted\n", n);
-#endif
-
-retry:
-		do {
-			int n_err;
-			int j;
-			int pos;
-			int x;
-
-#ifdef VERBOSE
-			printf("\nlfetch & lput\n");
-#endif
-			len = sizeof low;
-			ret = lower_fetch(low, &len);
-#ifdef VERBOSE
-			printf("ret=%d, len=%d, type=%d\n", ret, len, low[3]);
-			//hex_dump(stdout, low, len, 0, true);
-#endif
-		//printf("%d %d\n", len, low[3]);
-		//if (low[3] == 4 && len < 50)
-		//	hex_dump(stdout, low, len, 0, true);
-			if (!len)
-				break;
-
-#ifdef GEN_ERR
-			// generate err
-			x = len / 20; // err rate <= 5%
-			//x = len / 80;
-			if (x) {
-				n_err = random_range(x);	// num of err
-#ifdef VERBOSE
-				printf("n_err=%d <------------\n", n_err);
-#endif
-				for (j=0; j < n_err; ++j) {
-					pos = random_range(len);// random pos
-#ifdef VERBOSE
-					printf("pos=%02x, %02x->", pos, low[pos]);
-#endif
-					low[pos] = random_uint8();
-#ifdef VERBOSE
-					printf("%02x\n", low[pos]);
-#endif
-				}
-			}
-#endif
-
-			lower_put(low, len);
-		} while(1);
-
-		// simulate timer working proc
-		if (!get_urq_num() && !get_lsq_num())
-			proc_timer();
-		if (get_lsq_num() > 0) {
-			// if timer gen some pkts
-#ifdef VERBOSE
-			printf("timeout...and try again\n");
-#endif
-			goto retry;
-		}
-
-#ifdef VERBOSE
-		printf("\nrecv\n");
-#endif
-		len = sizeof r;
-		ret = upper_recv(r, &len);
-#ifdef VERBOSE
-		printf("ret=%d, len=%d\n", ret, len);
-		//hex_dump(stdout, r, len, 0, true);
-#endif
-		if (again && !len) {
-			++ fail;
-#ifdef VERBOSE
-			printf("len=0, fail=%d\n", fail);
-#endif
-			continue;
-		}
-
-		if (memcmp(s, r, n)) {
-			printf("orig(%d)---------------\n", n);
-			hex_dump(stdout, s, n, 0, true);
-			printf("err(%d)---------------\n", len);
-			hex_dump(stdout, r, len, 0, true);
-			printf("#####\n");
-			++ fail;
-#ifdef VERBOSE
-			printf("mismatch, fail=%d\n", fail);
-#endif
-		}
-		else {
-			// "ack all" may lost
-			if (again) {
-				proc_timer();
-				if (get_lsq_num() > 0) {
-					again = false;
-					goto retry;
-				}
-			}
-			again = true;
-		}
-	}
-	printf("\n\nfail=%d\n", fail);
-#ifdef EVALUATION
-	printf("valid=%llu, tot=%llu, ratio=%f\n", e_valid, e_tot, ((float)e_valid)/((float)e_tot));
-#endif
-
-	split_cleanup();
-=======
-#include <stdio.h>
-#include <pthread.h>
-#include "compact.h"
+/*
+ * ip stack -> tun ->
+ *	xmit_compress ->
+ *		split (upper_send, lower_fetch) ->
+ *			udp send -> ... -> (remote) udp recv ->
+ *		unsplit (lower_put, upper_recv) ->
+ *	recv_compress ->
+ * tun -> ip stack
+ */
 
 #define MAX_BUF_SIZE	2000
+
+static u32 tot = 1;
+static u32 full_ip = 0;
+static u32 compact_ip = 0;
+static u32 payload = 0;
 
 static void *recv_tun(void *arg)
 {
 	char buf[MAX_BUF_SIZE];
-	int n;
+	int n, n2;
 	pkt_t *pkt;
 	struct list pkt_list;
 
 	while (1) {
 		n = tun_read(buf, MAX_BUF_SIZE);
+		full_ip += n;
+		payload += n - 28;	// iphdr(20), udphdr(8)
 		//<<<<<<<<<< IP
-		//here
-		//>>>>>>>>>> compacted IP
+
 		xmit_compress(buf, &pkt_list);
-		LIST_FOR_EACH(pkt, node, &pkt_list)
-			udp_send(pkt->data, pkt->len);
+		//>>>>>>>>>> compacted IP
+		LIST_FOR_EACH(pkt, node, &pkt_list) {
+			n2 = upper_send(2, 1, pkt->data, pkt->len);
+			compact_ip += pkt->len;
+			if (n2 != n)
+				printf("upper_send error, ret=%d\n", n2);
+		}
+	}
+}
+
+static void *recv_split(void *arg)
+{
+	char buf[MAX_BUF_SIZE];
+	int n, n2;
+
+	while (1) {
+		n = MAX_BUF_SIZE;
+		n2 = lower_fetch(buf, &n);
+		if (n2 <= 0) {
+			usleep(500);
+			continue;
+		}
+
+		udp_send(buf, n2);
+		tot += n2;
 	}
 }
 
@@ -193,36 +77,69 @@ static void *recv_udp(void *arg)
 {
 	char buf[MAX_BUF_SIZE];
 	int n;
-	pkt_t *ippkt, *send_back_pkt;
 
 	while (1) {
 		n = udp_recv(buf, MAX_BUF_SIZE);
-		//<<<<<<<<<< compacted IP
-		//here
-		//>>>>>>>>>> IP
+		//<<<<<<<<<< splited & packed data frame
+
+		lower_put(buf, n);
+	}
+}
+
+static void *recv_unsplit(void *arg)
+{
+	char buf[MAX_BUF_SIZE];
+	int n, n2;
+	pkt_t *ippkt, *send_back_pkt;
+
+	while (1) {
+		n = MAX_BUF_SIZE;
+		n2 = upper_recv(buf, &n);
+		if (n2 <= 0) {
+			usleep(500);
+			continue;
+		}
+		//<<<<<<<<<< compacted ip
+
 		recv_compress(buf, n, &ippkt, &send_back_pkt);
 		if (ippkt)
 			tun_write(ippkt->data, ippkt->len);
 		if (send_back_pkt)
 			udp_send(send_back_pkt->data, send_back_pkt->len);
+
+		tun_write(buf, n2);
 	}
 }
 
 int main()
 {
-	pthread_t rtun, rudp;
+	pthread_t rtun, rsplit, rudp, runsplit;
 
 	compact_init();
+	split_init();
 
 	tun_init();
 	udp_init();
 
 	pthread_create(&rtun, NULL, recv_tun, NULL);
+	pthread_create(&rsplit, NULL, recv_split, NULL);
 	pthread_create(&rudp, NULL, recv_udp, NULL);
+	pthread_create(&runsplit, NULL, recv_unsplit, NULL);
 
-	while (1)
-		sleep(5);
->>>>>>> compact2
+	while (1) {
+		sleep(1);
+
+		proc_timer();
+
+		printf("effective payload = %d\n", payload);
+		printf("compact ip tot = %d\n", compact_ip);
+		printf("full ip tot = %d\n", full_ip);
+		printf("everything(including re-transmition), tot = %d\n", tot);
+		printf("data ratio(payload/tot) = %f\n", payload/tot);
+		printf("compact ratio(cip/fip) = %f\n", compact_ip/full_ip);
+		printf("split ratio(cip/tot) = %f\n", compact_ip/tot);
+		printf("-----\n\n");
+	}
 
 	return 0;
 }
